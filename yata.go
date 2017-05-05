064ed3bfff45ac88d0ec9613c2c62462de79a5de
Kenneth Bogner <bognerkenny@gmail.com>
2017-05-04 22:22:39 -0400

    Refactored code into YataTask and YataManager to separate concerns

--- yata.go
+++ yata.go
@@ -4,79 +4,183 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
 	"path"
-	"time"
 )
 
 const (
-	envTag     = "env"
-	defaultTag = "default"
+	// Low represents a low priority
+	Low = 1 << iota
+	// Med represents a medium priority
+	Med
+	// High represents a high priority
+	High
 )
 
-var (
-	ErrConfigNotLoaded = errors.New(fmt.Sprintf("Err: Could not load config"))
+const (
+	// DefaultProject represents the name of the default project when on is not specified
+	DefaultProject = "__none__"
+	// DefaultFilename represents the name of the default Yata tasks file
+	DefaultFilename = "main"
+	// ErrorPrefix represents the string placed before an encountered error
+	ErrorPrefix = "ERROR :: "
 )
-
-type Config struct {
-	Home    string `env:"HOME"`
-	BaseDir string `default:".todo"`
-	TodoDir string `default:"tasks"`
-}
-
-type MaybeTime struct {
-	Time  time.Time
-	Valid bool
-}
 
 type YataConfig struct {
 	BaseDir string
 }
 
+// YataManager handles all the task management
+type YataManager struct {
+	RootPath string
+	FileName string
+}
+
+// YataTask represents a yata task
 type YataTask struct {
-	Description string //`json:"desc"`
-	//created     time.Time `json:"created"`
-	//completed   MaybeTime `json:"done"`
-	Priority int    //`json:"priority"`
-	Project  string //`json:"project"`
+	Description string `json:"desc"`
+	Completed   bool   `json:"done"`
+	Priority    int    `json:"priority"`
+	Project     string `json:"project"`
 }
 
-func GetHomeEnv() string {
-	val, ok := os.LookupEnv("HOME")
-	if ok {
-		return val
+// NewSimpleYataTask creates a new Yata task with defaults
+func NewSimpleYataTask(description, project string) *YataTask {
+	return &YataTask{
+		Description: description,
+		Completed:   false,
+		Priority:    Med,
+		Project:     EitherString(project, DefaultProject),
 	}
-	panic(errors.New("Expected 'HOME' environment variable"))
 }
 
-func GetOr(key, fallback string) string {
-	val := os.Getenv(key)
-	if val == "" {
-		return fallback
+// NewLowPriorityYataTask creates a new low priority Yata task with defaults
+func NewLowPriorityYataTask(description, project string) *YataTask {
+	return &YataTask{
+		Description: description,
+		Completed:   false,
+		Priority:    Low,
+		Project:     EitherString(project, DefaultProject),
 	}
-	return val
 }
 
-func CreateRoot(rootPath string) {
-	_, err := os.Stat(rootPath)
+// NewHighPriorityYataTask creates a new high priority Yata task with defaults
+func NewHighPriorityYataTask(description, project string) *YataTask {
+	return &YataTask{
+		Description: description,
+		Completed:   false,
+		Priority:    High,
+		Project:     EitherString(project, DefaultProject),
+	}
+}
+
+// MarshalTask marshals the task into json bytes
+func (t *YataTask) MarshalTask() []byte {
+	json, err := json.Marshal(*t)
 	if err != nil {
-		os.Mkdir(rootPath, 0777)
+		log.Fatal(errors.New("Unable to marshal task"))
+	}
+	return json
+}
+
+// NewYataManager creates a new Yata manager
+func NewYataManager() *YataManager {
+	home, ok := os.LookupEnv("HOME")
+	if !ok {
+		panic(errors.New(ErrorPrefix + "Could not find 'HOME' environment variable"))
+	}
+	return &YataManager{
+		FileName: DefaultFilename,
+		RootPath: path.Join(home, ".yata"),
 	}
 }
 
-func CreateMainFile(rootPath, filename string) {
-	fullPath := path.Join(rootPath, filename)
+// InitializeDirectory will create the .yata directory if it does not already exist
+func (m *YataManager) InitializeDirectory() {
+	_, err := os.Stat(m.RootPath)
+	if err != nil {
+		os.Mkdir(m.RootPath, 0777)
+	}
+	m.CreateDefaultYataTasksFile()
+}
+
+// CreateDefaultYataTasksFile will create the default task file in the root
+func (m *YataManager) CreateDefaultYataTasksFile() {
+	m.CreateYataTasksFile("")
+}
+
+// CreateYataTasksFile will create a new file in the root for Yata tasks
+func (m *YataManager) CreateYataTasksFile(filename string) {
+	fullPath := path.Join(m.RootPath, EitherString(filename, DefaultFilename))
 	_, err := os.Stat(fullPath)
 	if err != nil {
-		os.Create(fullPath)
+		ioutil.WriteFile(fullPath, []byte("[]"), 0777)
 	}
+}
+
+// GetAllOpenTasks gets any open task found in the yata file
+func (m *YataManager) GetAllOpenTasks() (tasks []YataTask) {
+	var rawTasks []YataTask
+	dat := m.ReadFile()
+	err := json.Unmarshal(dat, &rawTasks)
+	CheckFatal(err)
+	for _, t := range rawTasks {
+		if !t.Completed {
+			tasks = append(tasks, t)
+		}
+	}
+	return tasks
+}
+
+// ReadFile reads the contents of the yata task file
+func (m *YataManager) ReadFile() []byte {
+	dat, err := ioutil.ReadFile(path.Join(m.RootPath, m.FileName))
+	CheckFatal(err)
+	return dat
+}
+
+// SaveNewTask will save the given task to the Yata file
+func (m *YataManager) SaveNewTask(t YataTask) {
+	tasks := m.GetAllOpenTasks()
+	tasks = append(tasks, t)
+	dat, err := json.Marshal(tasks)
+	CheckFatal(err)
+	ioutil.WriteFile(path.Join(m.RootPath, m.FileName), dat, 0777)
+}
+
+// CheckFatal displays a fatal log if
+func CheckFatal(err error) {
+	if err != nil {
+		log.Fatal(ErrorPrefix + err.Error())
+	}
+}
+
+// EitherString will return the first parameter if it is not nil; otherwise, it returns the second parameter
+func EitherString(s1, s2 string) string {
+	if s1 == "" {
+		return s2
+	}
+	return s1
+}
+
+func SeedFile(m *YataManager) {
+	t1 := NewSimpleYataTask("Task 1", "")
+	t2 := NewSimpleYataTask("Task 2", "")
+	t3 := NewSimpleYataTask("Task 3", "")
+	m.SaveNewTask(*t1)
+	m.SaveNewTask(*t2)
+	m.SaveNewTask(*t3)
 }
 
 func main() {
-	home := GetHomeEnv()
-	rootPath := path.Join(home, ".yata")
-	CreateRoot(rootPath)
-	CreateMainFile(rootPath, "yata")
+	ym := NewYataManager()
+	ym.InitializeDirectory()
+	SeedFile(ym)
+	tasks := ym.GetAllOpenTasks()
+	fmt.Printf("%+v", tasks)
+
 	if len(os.Args) <= 1 {
 		// TODO Show usage
 		os.Exit(1)
@@ -99,12 +203,12 @@ func main() {
 
 	switch args[0] {
 	case "add", "new":
-		simpleTask := &YataTask{"This is my first task", 2, "test"}
-		t, err := json.Marshal(simpleTask)
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(string(t))
+		//simpleTask := &YataTask{"This is my first task", 2, "test"}
+		//t, err := json.Marshal(simpleTask)
+		//if err != nil {
+		//	fmt.Println(err)
+		//}
+		//fmt.Println(string(t))
 		fmt.Println("New todo!")
 	case "config":
 		fmt.Println("Configuring")
