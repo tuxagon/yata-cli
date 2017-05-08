6b62afcebf81b848cb5e5419530cc704e2e225c4
Kenneth Bogner <bognerkenny@gmail.com>
2017-05-08 13:28:00 -0400

    Started using a cli library

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"os"
+	"time"
 	"yata-cli/yata"
+
+	"github.com/urfave/cli"
 )
 
 // func SeedFile(m *yata.Manager) {
@@ -15,6 +18,25 @@ import (
 // }
 
 func main() {
+	app := cli.NewApp()
+	app.Name = "yata"
+	app.Version = "0.1.0"
+	app.Compiled = time.Now()
+	app.Authors = []cli.Author{
+		cli.Author{
+			Name: "Kenneth Bogner",
+		},
+	}
+	app.Usage = "a command line task/todo manager"
+	app.Commands = []cli.Command{
+		cli.Command{
+			Name:        "list",
+			ShortName:   "ls",
+			Usage:       "[list] usage",
+			Description: "[list] description",
+		},
+	}
+	app.Run(os.Args)
 	m := yata.NewFileManager()
 	p := yata.NewCmdParser(m)
 	if len(os.Args) > 1 {
