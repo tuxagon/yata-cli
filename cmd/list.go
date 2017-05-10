c91ccce4c5115813955d87ca4593e49dbd5c141e
Kenneth Bogner <bognerkenny@gmail.com>
2017-05-09 20:34:38 -0400

    Started list cmd

--- cmd/list.go
+++ cmd/list.go
@@ -1,8 +1,13 @@
 package cmd
 
-import "github.com/urfave/cli"
+import (
+	"github.com/tuxagon/yata-cli/task"
+	"github.com/urfave/cli"
+)
 
 // List returns the list of tasks/todos that have been recorded
 func List(ctx *cli.Context) error {
+	m := task.NewFileManager()
+
 	return nil
 }
