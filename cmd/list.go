c968546fb9356e3caa0eefc36e705b6b13853a82
Kenneth Bogner <bognerkenny@gmail.com>
2017-05-09 20:59:51 -0400

    Added basic list cmd code

--- cmd/list.go
+++ cmd/list.go
@@ -1,12 +1,20 @@
 package cmd
 
 import (
+	"fmt"
+
+	"github.com/tuxagon/yata-cli/task"
 	"github.com/urfave/cli"
 )
 
 // List returns the list of tasks/todos that have been recorded
 func List(ctx *cli.Context) error {
-	//m := task.NewFileManager()
+	m := task.NewFileManager()
+	tasks := m.GetAllOpenTasks()
 
+	fmt.Println(tasks)
+	for _, v := range tasks {
+		fmt.Println(v)
+	}
 	return nil
 }
