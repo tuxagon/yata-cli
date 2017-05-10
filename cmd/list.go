425fbf3d5882cde7fa08249a76992c23d8285df3
Kenneth Bogner <bognerkenny@gmail.com>
2017-05-09 20:45:05 -0400

    Negative commit to get things building again

--- cmd/list.go
+++ cmd/list.go
@@ -1,13 +1,12 @@
 package cmd
 
 import (
-	"github.com/tuxagon/yata-cli/task"
 	"github.com/urfave/cli"
 )
 
 // List returns the list of tasks/todos that have been recorded
 func List(ctx *cli.Context) error {
-	m := task.NewFileManager()
+	//m := task.NewFileManager()
 
 	return nil
 }
